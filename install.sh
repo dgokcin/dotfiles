35256694dc6bb160040e7ea9cd252aef638461ed
Deniz Gökçin <dgokcin@gmail.com>
2020-05-19 02:13:07 +0300

    add os specific stuff placeholder

--- install.sh
+++ install.sh
@@ -20,6 +20,17 @@ cp .ideavimrc ~/
 cp .gvimrc ~/
 cp .zshrc ~/
 
+# os specific stuff
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    # Do something under GNU/Linux platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+fi
+
 cd ~/.vim_runtime
 
 echo 'set runtimepath+=~/.vim_runtime
