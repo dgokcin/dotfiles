48ce4e3956580659a07974729451a949a7125fea
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2020-05-20 11:17:48 +0300

    comment out os specific portion

--- install.sh
+++ install.sh
@@ -21,15 +21,15 @@ cp .gvimrc ~/
 cp .zshrc ~/
 
 # os specific stuff
-if [ "$(uname)" == "Darwin" ]; then
-    # Do something under Mac OS X platform
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Do something under GNU/Linux platform
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    # Do something under 32 bits Windows NT platform
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    # Do something under 64 bits Windows NT platform
-fi
+#if [ "$(uname)" == "Darwin" ]; then
+    ## Do something under Mac OS X platform
+#elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    ## Do something under GNU/Linux platform
+#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    ## Do something under 32 bits Windows NT platform
+#elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    ## Do something under 64 bits Windows NT platform
+#fi
 
 cd ~/.vim_runtime
 
