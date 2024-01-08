5861be2adb3b43bd2834e0258397013c0656780f
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-01-08 21:18:30 +0100

    refactor: restore init.lua config

--- nvim/lua/custom/plugins/nvim-comment.lua
+++ nvim/lua/custom/plugins/nvim-comment.lua
@@ -1,10 +1,12 @@
+local vim = vim
+
 return {
     'terrortylor/nvim-comment',
     config = function()
         require('nvim_comment').setup({
             create_mappings = true,
         })
-        vim.keymap.set("n", "<leader><space>", ":CommentToggle<CR>", { noremap = true })
-        vim.keymap.set("v", "<leader><space>", ":CommentToggle<CR>", { noremap = true })
+        vim.keymap.set("n", "<leader>c", ":CommentToggle<CR>", { noremap = true })
+        vim.keymap.set("v", "<leader>c", ":CommentToggle<CR>", { noremap = true })
     end
 }
