1c13cb17e200b5bc3295a263d2edc2e5d0c9ba99
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-01-12 14:35:19 +0100

    feat(nvim): add terraform and hcl to treesitter

--- nvim/lua/custom/plugins/substitute.lua
+++ nvim/lua/custom/plugins/substitute.lua
@@ -20,6 +20,7 @@ return {
       vim.keymap.set("n", "ss", require("substitute").line, { noremap = true })
       vim.keymap.set("n", "S", require("substitute").eol, { noremap = true })
       vim.keymap.set("x", "s", require("substitute").visual, { noremap = true })
+
       vim.keymap.set("n", "<leader>s", require("substitute.range").operator, { noremap = true })
       vim.keymap.set("x", "<leader>s", require("substitute.range").visual, { noremap = true })
       vim.keymap.set("n", "<leader>ss", require("substitute.range").word, { noremap = true })
