1054d7fe1551811484ab1927c7a6b9f4eff6bcc7
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-01-14 13:49:31 +0100

    fix: add BufRead to plugins for faster startup

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -1,6 +1,7 @@
 return {
     'kylechui/nvim-surround',
     version = '*',
+    event = 'BufRead',
     config = function()
         require("nvim-surround").setup({
             -- Configuration here, or leave empty to use defaults
