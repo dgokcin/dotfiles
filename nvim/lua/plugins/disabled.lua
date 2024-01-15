b8b39586dc45bad73c7953b3a94bd3fbefca9140
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-01-15 17:04:11 +0100

    fix: disable mini.ai plugin

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,5 +2,6 @@ return {
     -- disabled plugins
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     { "RRethy/vim-illuminate", enabled = false },
+    { "echasnovski/mini.ai", enabled = false },
     { "echasnovski/mini.surround", enabled = false }
   }
