d6962266598b61d619db7dc8a5061110e9c687b6
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-01-19 10:27:19 +0100

    fix(telescope-undo): fix preview height

--- nvim/lua/plugins/telesope-undo.lua
+++ nvim/lua/plugins/telesope-undo.lua
@@ -17,7 +17,11 @@ return {
     -- don't use `defaults = { }` here, do this in the main telescope spec
     extensions = {
       undo = {
-        -- telescope-undo.nvim config, see below
+        side_by_side = true,
+        layout_strategy = "vertical",
+        layout_config = {
+          preview_height = 0.8,
+        },
       },
       -- no other extensions here, they can have their own spec too
     },
