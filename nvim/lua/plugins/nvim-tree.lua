6ba9fda13e0b852437aa15a1bec320e87701ea67
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-01-19 10:26:49 +0100

    fix(nvim-tree): change open vertical split keymap

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -19,7 +19,7 @@ return {
         end
 
         api.config.mappings.default_on_attach(bufnr)
-        vim.keymap.set("n", "|", api.node.open.vertical, opts("Open: Vertical Split"))
+        vim.keymap.set("n", "\\", api.node.open.vertical, opts("Open: Vertical Split"))
         vim.keymap.set("n", "-", api.node.open.horizontal, opts("Open: Horizontal Split"))
       end
 
