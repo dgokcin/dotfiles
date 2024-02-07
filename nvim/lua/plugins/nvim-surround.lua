54c2e8f5156bbcafc771a3685818c5895a472b64
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-02-07 22:29:55 +0100

    style: remove unused keymaps

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -4,21 +4,6 @@ return {
   event = "BufRead",
   vscode = "true",
   config = function()
-    require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
-      keymaps = {
-        insert = "<C-g>s",
-        insert_line = "<C-g>S",
-        normal = "gs",
-        normal_cur = "gss",
-        normal_line = "gS",
-        normal_cur_line = "gSS",
-        visual = "S",
-        visual_line = "gS",
-        delete = "ds",
-        change = "cs",
-        change_line = "cS",
-      },
-    })
+    require("nvim-surround").setup({})
   end,
 }
