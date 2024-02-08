4dfb4cd7a25f8e5b0d64da016ba6f2c297037450
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-02-08 22:09:43 +0100

    style: remove trailing whitespace

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,4 +3,3 @@ return {
   { "akinsho/toggleterm.nvim", version = "*", event = "VeryLazy", config = true },
   vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true, desc = "Toggle Terminal" }),
 }
-
