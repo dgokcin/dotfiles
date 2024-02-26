7d97333f4ab9db1d13f0a5a44e105bf7fb7bc55a
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-02-26 09:16:50 +0100

    style: remove commented out code (#27)

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,5 +1,4 @@
 return {
-  -- amongst your other plugins
   { "akinsho/toggleterm.nvim", version = "*", event = "VeryLazy", config = true },
   vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true, desc = "Toggle Terminal" }),
 }
