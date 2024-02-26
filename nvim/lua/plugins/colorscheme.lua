1e33ce9f7b0d53532a1ef949c995f95bae19063f
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-02-26 09:16:50 +0100

    style: remove commented out code (#27)

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,3 @@
--- return {
---   {
---     "folke/tokyonight.nvim",
---     config = function()
---       require("tokyonight").setup({
---         transparent = true,
---         styles = {
---           sidebars = "transparent",
---           floats = "transparent",
---         },
---       })
---
---       -- Override the Visual highlight group after Vim starts up
---       vim.cmd([[
---         augroup MyColors
---           autocmd!
---           autocmd VimEnter * highlight Visual guibg=Gray guifg=#ffffff
---         augroup END
---       ]])
---     end,
---     opts = {
---       -- Your existing options
---     },
---   },
--- }
 return {
   -- add gruvbox
   { "Mofiqul/vscode.nvim" },
