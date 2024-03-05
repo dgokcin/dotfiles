3294ea7fa3c99ad87a7d3972f2b36d9e82a5123b
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-03-05 21:24:32 +0100

    feat: make fast-cursor-move configurable

---
+++ nvim/lua/plugins/fast-cursor-move.lua
@@ -0,0 +1,12 @@
+return {
+  dir = "~/codes/fast-cursor-move.nvim",
+  event = "BufRead",
+  vscode = true,
+  config = function()
+      vim.defer_fn(function()
+          require("fast-cursor-move").setup({
+              enable_acceleration = false,
+          })
+      end, 500)
+  end,
+}
