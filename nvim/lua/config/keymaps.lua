cee9499a72ede773dff3d5fa48054a71b4952296
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-03-05 11:28:26 +0100

    feat: customizing fast-cursor-move.nvim (#34)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,29 +3,30 @@
 -- Add any additional keymaps here
 
 -- Navigation
--- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true, desc = "Move up by display lines" })
-vim.keymap.set(
-  "n",
-  "j",
-  "v:count == 0 ? 'gj' : 'j'",
-  { expr = true, silent = true, desc = "Move down by display lines" }
-)
 -- Move between windows with shift + hjkl
 vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, desc = "Move to window left" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, desc = "Move to window right" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, desc = "Move to window below" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, desc = "Move to window above" })
 
+-- Warrior Mode
+vim.keymap.set("n", "<Up>", "<Nop>", { noremap = true, desc = "Disable arrow keys" })
+vim.keymap.set("n", "<Down>", "<Nop>", { noremap = true, desc = "Disable arrow keys" })
+vim.keymap.set("n", "<Left>", "<Nop>", { noremap = true, desc = "Disable arrow keys" })
+vim.keymap.set("n", "<Right>", "<Nop>", { noremap = true, desc = "Disable arrow keys" })
+
 -- Editing
 -- Clear search highlight with two <esc>
 vim.keymap.set("n", "<Esc><Esc>", ":noh<CR>", { noremap = true, silent = true, desc = "Clear search highlight" })
+
 -- Fix indentation in entire file
 vim.keymap.set("n", "<leader>cfi", "gg=G``zz", { noremap = true, desc = "Fix indentation in the entire file" })
 vim.keymap.set("v", "<leader>cfi", "gg=G``zz", { noremap = true, desc = "Fix indentation in the entire file" })
+
 -- Maps x and X to delete without yanking using register a
 vim.keymap.set("n", "x", '"ax', { noremap = true, desc = "Delete character without yanking" })
 vim.keymap.set("n", "X", '"aX', { noremap = true, desc = "Delete character before cursor without yanking" })
+
 -- Maps c and C to delete without yanking using register a
 vim.keymap.set("n", "c", '"ac', { noremap = true, desc = "Change character without yanking" })
 vim.keymap.set("n", "C", '"aC', { noremap = true, desc = "Change to end of line without yanking" })
@@ -39,6 +40,7 @@ vim.keymap.set("v", "p", '"_dP', { noremap = true, desc = "Paste over selection 
 -- Selection
 -- Maps ctrl + a to select all
 vim.keymap.set("n", "<C-A>", "ggVG", { noremap = true, desc = "Select All" })
+
 -- Maps <leader>w to visual in word
 vim.keymap.set("n", "<leader>w", "viw", { noremap = true, desc = "Select Word" })
 
@@ -46,22 +48,14 @@ vim.keymap.set("n", "<leader>w", "viw", { noremap = true, desc = "Select Word" }
 -- Makes <Tab> and <S-Tab> move to beginning/end of line in normal mode
 vim.keymap.set("n", "<Tab>", "$", { noremap = true, desc = "Move to end of line" })
 vim.keymap.set("n", "<S-Tab>", "^", { noremap = true, desc = "Move to start of line" })
+
 -- Insert blank line above/below without losing cursor position
 vim.keymap.set("n", "<Enter>", "m`o<Esc>``", { noremap = true, silent = true, desc = "Insert blank line below" })
 vim.keymap.set("n", "<BS>", "m`O<Esc>``", { noremap = true, silent = true, desc = "Insert blank line above" })
+
 -- Move current line 1 line down in v-line mode and remember cursor position with gv
-vim.api.nvim_set_keymap(
-  "v",
-  "J",
-  ":m '>+1<CR>gv=gv",
-  { noremap = true, silent = true, desc = "Move selected lines down" }
-)
-vim.api.nvim_set_keymap(
-  "v",
-  "K",
-  ":m '<-2<CR>gv=gv",
-  { noremap = true, silent = true, desc = "Move selected lines up" }
-)
+vim.api.nvim_set_keymap( "v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = "Move selected lines down" })
+vim.api.nvim_set_keymap("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = "Move selected lines up" })
 
 -- Terminal Mappings
 -- Escape terminal mode with <C-\\><C-n>
@@ -93,16 +87,8 @@ vim.keymap.set("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous
 -- File Explorer
 -- Toggle NvimTree
 vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle file explorer" })
-vim.keymap.set(
-  "n",
-  "<leader>e",
-  ":NvimTreeToggle<CR>",
-  { noremap = true, silent = true, desc = "Explorer NvimTree (root dir)" }
-)
+vim.keymap.set( "n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Explorer NvimTree (root dir)" })
 
--- Warrior Mode
-vim.keymap.set("n", "<Up>", "<Nop>", { noremap = true, desc = "Disable arrow keys" })
-vim.keymap.set("n", "<Down>", "<Nop>", { noremap = true, desc = "Disable arrow keys" })
-vim.keymap.set("n", "<Left>", "<Nop>", { noremap = true, desc = "Disable arrow keys" })
-vim.keymap.set("n", "<Right>", "<Nop>", { noremap = true, desc = "Disable arrow keys" })
-
+-- Delete some default keymaps
+vim.keymap.del("n", "j")
+vim.keymap.del("n", "k")
