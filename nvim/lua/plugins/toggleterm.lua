1f97473cb3472ed823e8bfc82e78a751b5cb417a
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-03-07 00:51:20 +0100

    fix: remove vscode from toggleterm plugin

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,6 @@ return {
     "akinsho/toggleterm.nvim",
     version = "*",
     event = "VeryLazy",
-    vscode = "true",
     config = true
   },
   vim.keymap.set( "n", "<leader>t", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true, desc = "Toggle Terminal" }),
