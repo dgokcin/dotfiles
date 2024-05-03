ed6a3d79ea0c22e2a783fe60ba8f16a70db82050
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-05-03 13:49:31 +0200

    refactor: update makefile structure and macros
    introduce pretty_print and install_with_brew macros for better output handling and package installation.
    
    update target names for clarity and consistency across makefiles.
    
    enhance error handling in symlink, mkdir_safe, and remove_file macros.

--- makefiles/gitconfigs.mk
+++ makefiles/gitconfigs.mk
@@ -1,12 +1,12 @@
 # Git configuration setups
 
-setup-personal-git:
+personal-git: ## Configure git with personal settings and email
 	$(call mkdir_safe,${HOME}/.config/git)
 	$(call mkdir_safe,${XDG_CONFIG_HOME}/git)
 	$(call symlink,base.gitconfig,${XDG_CONFIG_HOME}/git/config)
 	$(call symlink,personal.gitconfig,${HOME}/.gitconfig)
 
-setup-work-git:
+work-git: ## Configure git with work settings, email, and input preferences
 	$(call mkdir_safe,${HOME}/.config/git)
 	$(call symlink,base.gitconfig,${XDG_CONFIG_HOME}/git/config)
 	$(call symlink,work.gitconfig,${HOME}/.gitconfig)
