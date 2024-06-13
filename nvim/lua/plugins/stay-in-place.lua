58fcfcf3e84c0d4a31a05d6e72dec41dbf578607
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-06-13 15:07:26 +0200

    feat: add local opt variable and set wrap option in options.lua (#48)
    * feat: add local opt variable and set wrap option in options.lua
    
    * feat: enhance nvim-surround with which-key integration
    
    add detailed key mappings for surrounding text objects inside and around.
    
    include various text objects like quotes, brackets, and tags.
    
    adjust imports and configurations in lazy setup.
    
    * chore: clean up stay-in-place plugin file
    
    * fix: correct syntax and add missing newline
    
    correct syntax in .terminator.config by adding missing bracket.
    
    add missing newline at end of nvim/lua/plugins/flash.lua.
    
    update nvim-surround plugin to conditionally load which-key mappings based on vscode flag.
    
    update oil plugin keymap description for clarity.
    
    * fix: remove problematic iterm2 config file
    
    * fix: add en_US.UTF-8 locale in .path
    
    * modify iterm2 config for regerence only
    
    * fix: re-install homebrew for arm
    
    * feat: add nvm plugin to zshrc
    
    * docs: add aws guidelines and sam guidelines
    
    include detailed guidelines for working with AWS and using AWS SAM. add rules for security, performance, and cost optimization based on the AWS Well-Architected Framework.
    
    * docs: update lambda powertools usage in cursorrules
    
    * feat: add custom slash commands to .cursorrules

--- nvim/lua/plugins/stay-in-place.lua
+++ nvim/lua/plugins/stay-in-place.lua
@@ -1,9 +1,3 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-local vim = vim
-
 return {
   "gbprod/stay-in-place.nvim",
   event = "BufRead",
@@ -29,3 +23,4 @@ return {
     })
   end,
 }
+
