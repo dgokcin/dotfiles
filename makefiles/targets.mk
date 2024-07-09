4a362921abca83734bba0e88bdfd51d394e8d5ec
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-07-09 16:20:22 +0200

    ai tools (#54)
    * feat: add new ai tooling
    
    * chore: add aliases for fabric and code-insiders
    
    added aliases for the Copilot CLI command 'fabric' and the Visual Studio Code Insiders command 'code-insiders' to improve productivity and convenience.
    
    * docs: update prompt instructions
    
    Refine the prompt for 'conventional-commit' to include more detailed instructions on creating multiple commits for unrelated changes and ensuring each file change is appropriately added and committed.
    
    Remove unnecessary params attribute from 'create-issue' prompt.
    
    * fix: update model titles and remove redundant model
    
    update the title for the llama 3 model to 'ollama - llama 3'. remove the redundant 'Ollama' model entry. update the model name for 'Gemini 1.5 Pro' to 'gemini-pro'.
    
    update the prompt for the 'conventional-commit' custom command to remove unnecessary details and improve clarity.
    
    * fix: minor ai-tools changes

--- makefiles/targets.mk
+++ makefiles/targets.mk
@@ -1,7 +1,7 @@
 # High-level targets for setting up environments
 
 # Setup personal environment
-personal: vim vscodevim ideavim gvim nvim vsvim bash zsh personal-git yamllint
+personal: vim vscodevim ideavim gvim nvim vsvim bash zsh personal-git yamllint continue
 
 # Setup work environment
-work: vim vscodevim ideavim gvim nvim vsvim bash zsh work-git yamllint
+work: vim vscodevim ideavim gvim nvim vsvim bash zsh work-git yamllint continue
