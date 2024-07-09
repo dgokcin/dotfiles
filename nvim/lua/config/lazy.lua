f3337158e9807131a3fd88e689781a43c018dcbc
Deniz Gökçin <deniz.gokcin@devoteam.com>
2024-07-09 16:19:48 +0200

    fix: minor ai-tools changes

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.ui.alpha" },
     { import = "lazyvim.plugins.extras.vscode" },
