05c33725a240440609b3ef3eff79447f2244395e
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-07-14 18:52:27 +0200

    bye bye vim (#60)
    * chore: remove vim related configurations
    
    * feat: support motions for Tab and S-Tab mappings

--- makefiles/editors.mk
+++ makefiles/editors.mk
@@ -12,19 +12,6 @@ nvim: ## Setup Neovim configuration by linking dotfiles
 		fi; \
 	done
 
-vim: ## Setup Vim configuration and update submodules
-	@$(call symlink,.vim_runtime,${HOME}/.vim_runtime)
-	@$(call symlink,.vimrc,${HOME}/.vimrc)
-	@$(call mkdir_safe,${HOME}/.vim/view)
-	@git submodule init
-	@git submodule update
-
-vsvim: ## Setup VSVim configuration by linking .vsvimrc
-	$(call symlink,.vsvimrc,${HOME}/.vsvimrc)
-
-vscodevim: ## Setup VSCode Vim configuration by linking .vscodevimrc
-	$(call symlink,.vscodevimrc,${HOME}/.vscodevimrc)
-
 ideavim: ## Setup IdeaVim configuration by linking .ideavimrc
 	$(call symlink,.ideavimrc,${HOME}/.ideavimrc)
 
