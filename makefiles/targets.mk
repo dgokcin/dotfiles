53759828572402afd9791d48c6c7562bb4f70222
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-07-16 16:46:35 +0200

    feat: update .cursorrules (#61)
    - remove unneeded vimrc aliases

--- makefiles/targets.mk
+++ makefiles/targets.mk
@@ -1,9 +1,10 @@
 # High-level targets for setting up environments
 
 # Setup personal environment
-personal: vim vscodevim ideavim gvim nvim vsvim bash zsh personal-git yamllint continue
+personal: vscodevim ideavim gvim nvim vsvim bash zsh personal-git yamllint continue
 
 # Setup work environment
-work: vim vscodevim ideavim gvim nvim vsvim bash zsh work-git yamllint continue
+work: vscodevim ideavim gvim nvim vsvim bash zsh work-git yamllint continue
 
 PHONY: nvim
+
