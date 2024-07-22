4e822baec1421b3b8585417b734e7d659ceda514
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-07-22 23:08:37 +0200

    refactor: streamline nvim-surround keymaps and update clean targets

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -9,46 +9,35 @@ return {
       -- Setup which-key descriptions for nvim-surround keymaps
       local wk = require("which-key")
       local mappings = {
-        ys = {
-          name = "+surround",
-          ["i"] = {
-            name = "+inside",
-            ["\""] = "Surround inside double quotes",
-            ["'"] = "Surround inside single quotes",
-            ["("] = "Surround inside parentheses",
-            ["["] = "Surround inside brackets",
-            ["{"] = "Surround inside braces",
-            ["<"] = "Surround inside angle brackets",
-            ["`"] = "Surround inside backticks",
-            ["w"] = "Surround inside word",
-            ["W"] = "Surround inside WORD",
-            ["s"] = "Surround inside sentence",
-            ["p"] = "Surround inside paragraph",
-            ["t"] = "Surround inside tag block",
-          },
-          ["a"] = {
-            name = "+around",
-            ["\""] = "Surround around double quotes",
-            ["'"] = "Surround around single quotes",
-            ["("] = "Surround around parentheses",
-            ["["] = "Surround around brackets",
-            ["{"] = "Surround around braces",
-            ["<"] = "Surround around angle brackets",
-            ["`"] = "Surround around backticks",
-            ["w"] = "Surround around word",
-            ["W"] = "Surround around WORD",
-            ["s"] = "Surround around sentence",
-            ["p"] = "Surround around paragraph",
-            ["t"] = "Surround around tag block",
-          }
-        }
+        { mode = { "n", "v" } },
+        { "ys", group = "surround" },
+        { "ysa", group = "around" },
+        { 'ysa"', desc = "Surround around double quotes" },
+        { "ysa'", desc = "Surround around single quotes" },
+        { "ysa(", desc = "Surround around parentheses" },
+        { "ysa<", desc = "Surround around angle brackets" },
+        { "ysaW", desc = "Surround around WORD" },
+        { "ysa[", desc = "Surround around brackets" },
+        { "ysa`", desc = "Surround around backticks" },
+        { "ysap", desc = "Surround around paragraph" },
+        { "ysas", desc = "Surround around sentence" },
+        { "ysat", desc = "Surround around tag block" },
+        { "ysaw", desc = "Surround around word" },
+        { "ysa{", desc = "Surround around braces" },
+        { "ysi", group = "inside" },
+        { 'ysi"', desc = "Surround inside double quotes" },
+        { "ysi'", desc = "Surround inside single quotes" },
+        { "ysi(", desc = "Surround inside parentheses" },
+        { "ysi<", desc = "Surround inside angle brackets" },
+        { "ysiW", desc = "Surround inside WORD" },
+        { "ysi[", desc = "Surround inside brackets" },
+        { "ysi`", desc = "Surround inside backticks" },
+        { "ysip", desc = "Surround inside paragraph" },
+        { "ysis", desc = "Surround inside sentence" },
+        { "ysit", desc = "Surround inside tag block" },
+        { "ysiw", desc = "Surround inside word" },
+        { "ysi{", desc = "Surround inside braces" },
       }
-
-      local opts = {
-        prefix = "",
-        mode = { "n", "v" },  -- Normal and Visual mode
-      }
-      wk.register(mappings, opts)
     end
   end,
 }
