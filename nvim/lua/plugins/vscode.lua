6e09121a0f3a1725a847a13f07665c315c0bc6e7
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-08-12 10:54:05 +0200

    feat(vscode): refactor vscode spevific keymaps (#89)
    - removed vscode plugin import from lazy setup
    - created autocmd for lazyvim keymaps defaults to set vscode-specific keymaps
    - added conditional return for non-vscode environments
    - updated keymaps for undo/redo, terminal toggle, search, and tab navigation
    
    BEGIN_NESTED_COMMIT
    fix: remove commented-out disabled plugins
    END_NESTED_COMMIT

--- nvim/lua/plugins/vscode.lua
+++ nvim/lua/plugins/vscode.lua
@@ -1,20 +1,58 @@
-return {
-    "LazyVim/LazyVim",
-    vscode = true,
-    opts = function(_, opts)
-        -- print something so that we know that the plugin is loaded
-        print("VSCode plugin loaded")
+if not vim.g.vscode then
+    return {}
+  end
 
-        -- Set VSCode-specific keymaps after a short delay
-        vim.defer_fn(function()
-            -- Undo/Redo Correction for VSCode
-            vim.keymap.set("n", "u", "<Cmd>call VSCodeNotify('undo')<CR>")
-            vim.keymap.set("n", "<C-r>", "<Cmd>call VSCodeNotify('redo')<CR>")
-            vim.keymap.set("n", "<leader>t", "<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>")
+  local enabled = {
+    "flit.nvim",
+    "lazy.nvim",
+    "leap.nvim",
+    "nvim-treesitter",
+    "nvim-treesitter-textobjects",
+    "nvim-ts-context-commentstring",
+    "ts-comments.nvim",
+    "vim-repeat",
+    "yanky.nvim",
+    "LazyVim",
+  }
 
-            -- Tab navigation for VSCode
-            vim.keymap.set("n", "<S-h>", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
-            vim.keymap.set("n", "<S-l>", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
-        end, 100)  -- 100ms delay
+  local Config = require("lazy.core.config")
+  Config.options.checker.enabled = false
+  Config.options.change_detection.enabled = false
+  Config.options.defaults.cond = function(plugin)
+    return vim.tbl_contains(enabled, plugin.name) or plugin.vscode
+  end
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyVimKeymapsDefaults",
+    callback = function()
+      -- Undo/Redo Correction for VSCode
+      vim.keymap.set("n", "u", "<Cmd>call VSCodeNotify('undo')<CR>")
+      vim.keymap.set("n", "<C-r>", "<Cmd>call VSCodeNotify('redo')<CR>")
+      vim.keymap.set("n", "<leader>t", "<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>")
+
+      -- VSCode-specific keymaps for search and navigation
+      vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
+      vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
+      vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
+
+      -- Tab navigation for VSCode
+      vim.keymap.set("n", "<S-h>", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
+      vim.keymap.set("n", "<S-l>", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
     end,
-}
+  })
+
+  return {
+    {
+      "LazyVim/LazyVim",
+      config = function(_, opts)
+        opts = opts or {}
+        -- disable the colorscheme
+        opts.colorscheme = function() end
+        require("lazyvim").setup(opts)
+      end,
+    },
+    {
+      "nvim-treesitter/nvim-treesitter",
+      opts = { highlight = { enable = false } },
+    },
+  }
