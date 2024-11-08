fb399ea11de783031d08ac2f54d4235d84599f6e
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-11-08 16:26:34 +0100

    feat: add  to multi cursor event types (#134)

--- nvim/lua/plugins/vim-visual-multi.lua
+++ nvim/lua/plugins/vim-visual-multi.lua
@@ -2,7 +2,7 @@ return {
   "mg979/vim-visual-multi",
   branch = "master",
   vscode = "true",
-  event = "BufReadPost",
+  event = { "BufReadPost", "BufEnter" },
   init = function()
     vim.g.VM_maps = {
       ["Add Cursor Down"] = "<M-j>",
@@ -10,4 +10,3 @@ return {
     }
   end,
 }
-
