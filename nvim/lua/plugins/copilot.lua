475a76b30c82cd1d04d51515510e0e6897bf6f9e
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2024-12-02 21:48:17 +0100

    major plugin update(#139)
    * feat(git): add hello alias for greeting message
    
    * feat: add smear-cursor
    
    * fix: resolve weird vscode error after update
    
    * feat: add some work related aliases
    
    * chore: update work related git settings
    
    * fix: fix for ai stuff
    
    ---------
    
    Co-authored-by: Deniz Gökçin <deniz.gokcin@devoteam.com>
    Co-authored-by: Deniz Gökçin <deniz.gokcin@treatwell.com>

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,10 +1,18 @@
 return {
   "zbirenbaum/copilot.lua",
-  enabled = true,
   cmd = "Copilot",
-  event = "VeryLazy",
+  build = ":Copilot auth",
+  event = "InsertEnter",
   opts = {
-    suggestion = { enabled = false },
+    suggestion = {
+      enabled = not vim.g.ai_cmp,
+      auto_trigger = true,
+      keymap = {
+        accept = false, -- handled by nvim-cmp / blink.cmp
+        next = "<M-]>",
+        prev = "<M-[>",
+      },
+    },
     panel = { enabled = false },
     filetypes = {
       gitcommit = true,
--- nvim/lua/plugins/vscode.lua
+++ nvim/lua/plugins/vscode.lua
@@ -1,67 +1,75 @@
 if not vim.g.vscode then
-    return {}
-  end
+  return {}
+end
 
-  local enabled = {
-    "flit.nvim",
-    "lazy.nvim",
-    "leap.nvim",
-    "nvim-treesitter",
-    "nvim-treesitter-textobjects",
-    "nvim-ts-context-commentstring",
-    "ts-comments.nvim",
-    "vim-repeat",
-    "yanky.nvim",
-    "LazyVim",
-  }
+local enabled = {
+  "LazyVim",
+  "dial.nvim",
+  "flit.nvim",
+  "lazy.nvim",
+  "leap.nvim",
+  "mini.ai",
+  "mini.comment",
+  "mini.move",
+  "mini.pairs",
+  "mini.surround",
+  "nvim-treesitter",
+  "nvim-treesitter-textobjects",
+  "nvim-ts-context-commentstring",
+  "snacks.nvim",
+  "ts-comments.nvim",
+  "vim-repeat",
+  "yanky.nvim",
+}
 
-  local Config = require("lazy.core.config")
-  Config.options.checker.enabled = false
-  Config.options.change_detection.enabled = false
-  Config.options.defaults.cond = function(plugin)
-    return vim.tbl_contains(enabled, plugin.name) or plugin.vscode
-  end
+local Config = require("lazy.core.config")
+Config.options.checker.enabled = false
+Config.options.change_detection.enabled = false
+Config.options.defaults.cond = function(plugin)
+  return vim.tbl_contains(enabled, plugin.name) or plugin.vscode
+end
 
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "LazyVimKeymapsDefaults",
-    callback = function()
-      -- Undo/Redo Correction for VSCode
-      vim.keymap.set("n", "u", "<Cmd>call VSCodeNotify('undo')<CR>")
-      vim.keymap.set("n", "<C-r>", "<Cmd>call VSCodeNotify('redo')<CR>")
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyVimKeymapsDefaults",
+  callback = function()
+    -- Undo/Redo Correction for VSCode
+    vim.keymap.set("n", "u", "<Cmd>call VSCodeNotify('undo')<CR>")
+    vim.keymap.set("n", "<C-r>", "<Cmd>call VSCodeNotify('redo')<CR>")
 
-      -- Toggle Terminal
-      vim.keymap.set("n", "<leader>t", "<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>")
+    -- Toggle Terminal
+    vim.keymap.set("n", "<leader>t", "<Cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>")
 
-      -- VSCode-specific keymaps for search and navigation
-      vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
-      vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
-      vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
+    -- VSCode-specific keymaps for search and navigation
+    vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
+    vim.keymap.set("n", "<leader>/", [[<cmd>call VSCodeNotify('workbench.action.findInFiles')<cr>]])
+    vim.keymap.set("n", "<leader>ss", [[<cmd>call VSCodeNotify('workbench.action.gotoSymbol')<cr>]])
 
-      -- Tab navigation for VSCode
-      vim.keymap.set("n", "<S-h>", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
-      vim.keymap.set("n", "<S-l>", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
+    -- Tab navigation for VSCode
+    vim.keymap.set("n", "<S-h>", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
+    vim.keymap.set("n", "<S-l>", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
 
-      -- Toggle Primary Sidebar Like nvim-tree
-      vim.keymap.set("n", "<leader>e", "<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>")
+    -- Toggle Primary Sidebar Like nvim-tree
+    vim.keymap.set("n", "<leader>e", "<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>")
 
-      -- Disable convert to uppercase and lowercase in visual mode
-      vim.keymap.set('v', 'u', '<Esc>', { noremap = true, silent = true })
-      vim.keymap.set('v', 'U', '<Esc>', { noremap = true, silent = true })
+    -- Disable convert to uppercase and lowercase in visual mode
+    vim.keymap.set("v", "u", "<Esc>", { noremap = true, silent = true })
+    vim.keymap.set("v", "U", "<Esc>", { noremap = true, silent = true })
+  end,
+})
+
+return {
+  {
+    "LazyVim/LazyVim",
+    config = function(_, opts)
+      opts = opts or {}
+      -- disable the colorscheme
+      opts.colorscheme = function() end
+      require("lazyvim").setup(opts)
     end,
-  })
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { highlight = { enable = false } },
+  },
+}
 
-  return {
-    {
-      "LazyVim/LazyVim",
-      config = function(_, opts)
-        opts = opts or {}
-        -- disable the colorscheme
-        opts.colorscheme = function() end
-        require("lazyvim").setup(opts)
-      end,
-    },
-    {
-      "nvim-treesitter/nvim-treesitter",
-      opts = { highlight = { enable = false } },
-    },
-  }
